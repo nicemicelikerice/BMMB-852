1d0f913d7d1283e82e1c3de84e93f4b1822170a2
nicemicelikerice <www.goIke@gmail.com>
2024-10-09 13:34:37 -0400

    Update HW6.sh

--- HW6/HW6.sh
+++ HW6/HW6.sh
@@ -7,7 +7,7 @@ set -uex
 ##--------Change This Section to Update the File--------##
 
 #Set SRR
-srr="SRR22942457"
+srr="SRR639755"
 
 #Set number of reads
 N=10000
@@ -40,3 +40,13 @@ fastq-dump -X ${N} -F --outdir reads --split-files ${srr}
 #Run FastQC
 fastqc -q ${r1} ${r2} -o ${pdir}
 
+#QC Trimming
+fastp --adapter_sequence=${ADAPTER} --cut_tail \
+      -i ${r1} -I ${r2} -o ${t1} -O ${t2}
+
+#Run FastQC on trimmed reads
+fastqc -q -o ${pdir} ${t1} ${t2}
+
+#Assuming that the environment menv has been created
+#Create multiqc report
+micromamba run -n menv multiqc -o ${pdir} ${pdir}
